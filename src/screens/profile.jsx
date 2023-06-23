f1d6189f19d70591cfb385f4ca0c798617ee735a
FranciscoPedicino <franciscopedi91@gmail.com>
2023-06-22 21:40:54 -0300

    photo

--- src/screens/profile.jsx
+++ src/screens/profile.jsx
@@ -18,6 +18,8 @@ const Profile = () => {
   const [token, setToken] = useState(null);
   const isFocused = useIsFocused();
   let email = JSON.parse(localStorage.getItem("user"))?.email;
+  let photo = JSON.parse(localStorage.getItem("user"))?.photo;
+
   const checkToken = async () => {
     try {
       const storedToken = await AsyncStorage.getItem('token');
@@ -47,7 +49,7 @@ const Profile = () => {
 
   return (
     <ImageBackground source={b} style={styles.container}>
-        <Image></Image>
+        <Image source={photo} style={{width:100,height:100,marginBottom:100,borderRadius:50}}></Image>
         <Text style={{fontSize:15,fontWeight:'bold',color:'black',backgroundColor: 'rgba(255, 255, 255, 0.6)',borderRadius:12,padding:20,width:'70%',height:5, textAlign: 'center',marginBottom:30}}>{email}</Text>
         <TouchableOpacity onPress={logout} style={{ flex: 0.1, justifyContent: 'center', alignItems: 'center', alignContent: 'center', backgroundColor: 'white', height: 25, width: 200, borderRadius: 15, marginTop: 10 }}>
         <Text style={styles.buttonText}>logout</Text>
